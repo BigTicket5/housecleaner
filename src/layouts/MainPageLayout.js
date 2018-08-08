bd25c9f6694ef75e0b9c87af725750a0fa425f5a
Zhenzhong Mao <dkrose.random@gmail.com>
2018-08-08 17:45:35 +0800

    20180808_1

--- src/layouts/MainPageLayout.js
+++ src/layouts/MainPageLayout.js
@@ -1,20 +1,19 @@
 import React from "react";
-import {Route,Switch,Redirect,Link} from "react-router-dom";
+import {Route,Switch,Redirect} from "react-router-dom";
 import UserPageLayout from './UserPageLayout';
 import ServicePageLayout from './ServicePageLayout';
+import HeaderMenu from './menus/HeaderMenu';
 import HomePage from '../pages/HomePage'
 class MainPageLayout extends React.Component{
     render(){
         return(
             <div>
-                <Link to="/">Home</Link>
-                <Link to="/user">Profile</Link>
-                <Link to="/services">Service</Link>
+                <HeaderMenu/>
                 <main>
                     <Switch>
                         <Route path="/" exact component = {HomePage}/>
-                        <Route path="/services" component = {ServicePageLayout}/>
                         <Route path="/user" component={UserPageLayout} />
+                        <Route path="/services" component = {ServicePageLayout}/>
                         <Redirect to="/" />
                     </Switch>
                 </main>
